ad5a0442eedfafef4004797475a4813c1d5561c6
God-Luffy <143315093+God-Luffy@users.noreply.github.com>
2023-12-08 19:16:33 +0530

    Update smart-enhancer.py

--- smart-enhancer.py
+++ smart-enhancer.py
@@ -13,7 +13,7 @@ import asyncio
 from aiogram import Bot, Dispatcher, types
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
-bot = Bot(token='YOUR_TOKEN_HERE')   # REPLACE YOUR TELEGRAM BOT TOKEN HERE
+bot = Bot(token='6357383464:AAHBE2byrVIKS6eiZEyw923uhSMKIxSmwW0')   # REPLACE YOUR TELEGRAM BOT TOKEN HERE
 dp = Dispatcher(bot)
 
 API_KEY = "REMINI_API_KEY"   # REPLACE YOUR REMINI API KEY THAT HAS CREDITS
